7460c3d516155d046a5fc317e5a79e0d2df3feff
hugedong69 <pentagonalpentagon@gmail.com>
2017-07-28 18:03:18 +0800

    i fix
    Apparently S_GUILD_NAME also loads emblem because why not

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 module.exports = function DeletEmblem(dispatch) {
+
 	dispatch.hook('S_SPAWN_USER', 5, event =>{
-	event.guildEmblem = '';
+	event.guildEmblem = ''
 	return true;
+	})
+	dispatch.hook('S_GUILD_NAME', 1, event =>{
+		event.guildEmblem = ''
+		return true;
 	})
 }
